712f3704aaafb39bc62ee580036dd6b6206ba2db
Bruno Carazato <brunocarazatoo@gmail.com>
2024-09-02 20:04:22 -0300

    Cursos Controller

---
+++ LPADS2024T2/Controllers/CursosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LPADS2024T2.Controllers
+{
+    public class CursosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
