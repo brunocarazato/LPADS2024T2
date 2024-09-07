3b679e914729cf01900296d062960ffd2527c827
Bruno Carazato <brunocarazatoo@gmail.com>
2024-09-06 22:24:39 -0300

    comments

--- LPADS2024T2/Data/ConnectionContext.cs
+++ LPADS2024T2/Data/ConnectionContext.cs
@@ -5,7 +5,16 @@ using Microsoft.EntityFrameworkCore;
     public class ConnectionContext : DbContext
     {
 
-        public DbSet<Aluno> Alunos { get; set; }
+    /*  
+     *   CREATE LOGIN novoUsuario WITH PASSWORD = 'SenhaSegura123!';
+     *   USE LPADS2024T2;
+     *   GO
+     *
+     *   CREATE USER novoUsuario FOR LOGIN novoUsuario;
+
+     *   ALTER ROLE db_owner ADD MEMBER novoUsuario;
+     */
+    public DbSet<Aluno> Alunos { get; set; }
         public DbSet<Curso> Cursos { get; set; }
 
         public ConnectionContext(DbContextOptions<ConnectionContext> options) : 
